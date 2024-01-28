5b24de7f6c9633e3c8e61d18c540d02d33860074
Oto Avlokhashvili <otar.avlokhashvili@aidnet.ge>
2024-01-28 12:44:57 +0400

    feat-final-version

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { FlowersComponent } from './flowers/flowers.component';
+const routes: Routes = [
+/*   {path: '',pathMatch: 'full',redirectTo: 'home'},
+  {path:'home',component:MainComponent},
+  {path:'flowers',component:FlowersComponent}, */
+
+
+
+
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
