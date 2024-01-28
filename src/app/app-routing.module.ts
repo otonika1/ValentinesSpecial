b8a6d630a24f46800da96ab3f7afbeec664e0be7
Oto Avlokhashvili <otar.avlokhashvili@aidnet.ge>
2024-01-28 13:03:45 +0400

    feat-final

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { MainComponent } from './main/main.component';
-import { FlowersComponent } from './flowers/flowers.component';
 const routes: Routes = [
 /*   {path: '',pathMatch: 'full',redirectTo: 'home'},
   {path:'home',component:MainComponent},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
 import { AppRoutingModule } from './app-routing.module';
-import { FlowersComponent } from './flowers/flowers.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
-    FlowersComponent
   ],
   imports: [
     BrowserModule,
